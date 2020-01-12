279c6b8d164962e7e15835a5dac33bb945055527
Lizz1102 <lizz05110@gmail.com>
2020-01-12 00:11:35 -0500

    Implemented order placement

--- SketchShop/Models/ApplicationDbContext.cs
+++ SketchShop/Models/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace SketchShop.Models
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 : base(options) { }
 		public DbSet<Product> Products { get; set; }
+		public DbSet<Order> Orders { get; set; }
 	}
 }
